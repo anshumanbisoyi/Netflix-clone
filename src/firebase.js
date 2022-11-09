5841e8e32186864d3bc088495463629a2852b69d
Anshuman Bisoyi <ansuman746@gmail.com>
2022-11-09 22:51:31 +0530

    intial

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCN7ZDkziuqSiqLwI9hLBKHn2DQ3_SgEK0",
+    authDomain: "netflix-clone-9e356.firebaseapp.com",
+    projectId: "netflix-clone-9e356",
+    storageBucket: "netflix-clone-9e356.appspot.com",
+    messagingSenderId: "286998187741",
+    appId: "1:286998187741:web:43d996537729f69e7b5534"
+  };

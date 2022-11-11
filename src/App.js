fd9bea8d1a2656414308e4397f29df71f3e3a705
Anshuman Bisoyi <ansuman746@gmail.com>
2022-11-12 04:11:20 +0530

    add profilescreen

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
-import { 
-  BrowserRouter as Router, 
-  Routes,
-   Route 
-  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "./features/counter/userSlice";
+import { login } from "./features/counter/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 function App() {
-  const user=null;
-  // {
-  //   name:"Anshuman"
-  // };
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    const unsubscribe=auth.onAuthStateChanged(userAuth => {
-      if(userAuth){
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
         //loggedIn
         console.log(userAuth);
-      }else{
-       //loggedOut
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
+      } else {
+        //loggedOut
+        dispatch(logout());
       }
     });
-    return () =>{
+    return () => {
       unsubscribe();
-    }
-  },[]); 
+    };
+  }, [dispatch]);
 
   return (
     <div className="app">
@@ -35,6 +38,7 @@ function App() {
           <LoginScreen />
         ) : (
           <Routes>
+            <Route path="/profile" element={<ProfileScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         )}
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,9 @@
 import React,{useEffect, useState} from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import "./Nav.css";
 function Nav() {
     const [show,handleShow]=useState(false);
+    const navigate = useNavigate();
     const transitionNavBar=()=>{
         if(window.scrollY > 100){
             handleShow(true);
@@ -19,11 +21,13 @@ function Nav() {
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
         <img
+        onClick={()=>navigate("/")}
           className="nav_logo"
           src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
           alt="netflix logo"
         />
         <img
+          onClick={() => navigate("/profile")}
           className="nav_avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt="avatar logo"

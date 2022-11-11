de2e0a4022556923f01a5879fdad18f66bac45ad
Anshuman Bisoyi <ansuman746@gmail.com>
2022-11-11 22:27:00 +0530

    connect firebase auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { 
@@ -7,11 +7,26 @@ import {
    Route 
   } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 function App() {
   const user=null;
   // {
   //   name:"Anshuman"
   // };
+
+  useEffect(()=>{
+    const unsubscribe=auth.onAuthStateChanged(userAuth => {
+      if(userAuth){
+        //loggedIn
+        console.log(userAuth);
+      }else{
+       //loggedOut
+      }
+    });
+    return () =>{
+      unsubscribe();
+    }
+  },[]); 
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCN7ZDkziuqSiqLwI9hLBKHn2DQ3_SgEK0",
-    authDomain: "netflix-clone-9e356.firebaseapp.com",
-    projectId: "netflix-clone-9e356",
-    storageBucket: "netflix-clone-9e356.appspot.com",
-    messagingSenderId: "286998187741",
-    appId: "1:286998187741:web:43d996537729f69e7b5534"
-  };
+  apiKey: "AIzaSyCN7ZDkziuqSiqLwI9hLBKHn2DQ3_SgEK0",
+  authDomain: "netflix-clone-9e356.firebaseapp.com",
+  projectId: "netflix-clone-9e356",
+  storageBucket: "netflix-clone-9e356.appspot.com",
+  messagingSenderId: "286998187741",
+  appId: "1:286998187741:web:43d996537729f69e7b5534",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
